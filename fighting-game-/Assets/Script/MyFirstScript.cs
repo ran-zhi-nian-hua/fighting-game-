4b9c254ad7c0a1e48a7ca13d03c5bd399294dc2c
ran-zhi-nian-hua <2848476675@qq.com>
2025-07-30 22:44:47 +0800

    upload

---
+++ fighting-game-/Assets/Script/MyFirstScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    public float JumpForce = 6.0f;
+    public Rigidbody2D CharacterRig;
+    public Animator CharacterAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float XInput = Input.GetAxisRaw("Horizontal");
+        float YInput = Input.GetAxisRaw("Vertical");
+        CharacterRig.velocity = new Vector2(XInput*Speed,CharacterRig.velocity.y);
+        if(XInput!=0)
+        {
+            CharacterAni.SetBool("ISRun", true);
+        }
+        else
+        {
+            CharacterAni.SetBool("ISRun", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CharacterRig.velocity = new Vector2(CharacterRig.velocity.x, JumpForce);
+        }
+    }
+}
